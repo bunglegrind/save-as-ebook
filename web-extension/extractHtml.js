2d4df682779e86fa08861f57bfc199eb8beb877d
bunglegrind <bunglegrind@libero.it>
2022-07-11 23:30:12 +0200

    added font tag

--- web-extension/extractHtml.js
+++ web-extension/extractHtml.js
@@ -12,7 +12,7 @@ var extractedImages = [];
 const allowedTags = [
     "blockquote", "menu",
     "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4", "h5", "h6",
-    "hgroup", "nav", "section", "dd", "div", "dl", "dt", "figcaption", "figure", "hr", "li",
+    "hgroup", "nav", "section", "dd", "div", "dl", "dt", "figcaption", "figure", "font", "hr", "li",
     "main", "ol", "p", "pre", "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data",
     "dfn", "em", "i", "img", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "small", "span",
     "strong", "sub", "sup", "time", "u", "var", "wbr", "del", "ins", "caption", "col", "colgroup",
